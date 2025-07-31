dea989735619baa9d2168a9c90e694a0a7ce490b
Samuel Molina <samumoe2401@gmail.com>
2025-07-30 19:58:12 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.HTML')
-    return render(request, 'home.html', {'name': 'Samuel Molina'})
+    #return render(request, 'home.html', {'name': 'Samuel Molina'})
+    #searchTerm = request.GET.get('searchMovie')
+    #movies = Movie.objects.all()
+    #return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies}  )
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'movies': movies})
 
 def about(request):
 
