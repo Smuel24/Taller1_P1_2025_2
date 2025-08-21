1647f97ef461d9c03b1018406524d3b24fda93e1
Samuel Molina <samumoe2401@gmail.com>
2025-08-20 22:46:29 -0500

    Cards

--- movie/views.py
+++ movie/views.py
@@ -4,18 +4,17 @@ from django.http import HttpResponse
 from .models import Movie
 
 def home(request):
-    #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    #return render(request, 'home.HTML')
-    #return render(request, 'home.html', {'name': 'Samuel Molina'})
-    #searchTerm = request.GET.get('searchMovie')
-    #movies = Movie.objects.all()
-    #return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies}  )
     searchTerm = request.GET.get('searchMovie')
+    
     if searchTerm:
         movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies = Movie.objects.all()
-    return render(request, 'home.html', {'movies': movies})
+
+    return render(request, 'home.html', {
+        'movies': movies,
+        'searchTerm': searchTerm
+    })  
 
 def about(request):
 
